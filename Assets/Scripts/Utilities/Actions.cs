0f2a099a8685d6f25a6a3c83556b4e5d6fd295da
Dylan_Schumacker <dylan.schumacker@outlook.fr>
2022-05-10 09:42:03 +0200

    Revert "*.*"
    This reverts commit 1d7e1f9331d202dc47ef8ec68a618c5fa55180e1.

---
+++ Assets/Scripts/Utilities/Actions.cs
@@ -0,0 +1,45 @@
+using System;
+using UnityEngine;
+
+namespace Khynan_Coding
+{
+    public static class Actions
+    {
+        public static Action OnGameStateChanged;
+        public static Action OnGamePaused;
+
+        public static Action<Weapon> OnInitializingWeapon;
+        public static Action<Weapon, int> OnEquippingWeapon;
+
+        public static Action<Weapon> OnShooting;
+
+        public static Action OnReloadStarted;
+        public static Action<Weapon> OnReloadStartedSetWeaponData;
+        public static Action<Weapon> OnReloadEndedSetWeaponData;
+        public static Action OnReloadEnded;
+
+        public static Action<CharacterType> OnAimingInteractable;
+
+        public static Action<int> OnAddingScore;
+
+        public static Action OnHittingValidTarget;
+        public static Action OnEnemyDeath;
+
+        public static Action<int> OnKillCountValueChanged;
+        public static Action OnKillCountStepReached;
+
+        public static Action<int> OnGlobalScoreValueChanged;
+
+        public static Action<float, float> OnPlayerHealthValueInitialized;
+        public static Action<float, float, HealthInteraction> OnPlayerHealthValueChanged;
+
+        public static Action<StatsManager> OnEnemyHealthValueChanged;
+
+        public static Action OnWaveBegun;
+        public static Action OnWaveEnded;
+        public static Action<int> OnWaveCountValueChanged;
+
+        public static Action<Transform, InteractionData> OnPlayerInteractionPossible;
+        public static Action OnPlayerInteractionImpossible;
+    }
+}
---
+++ Assets/Scripts/Utilities/AnimatorHelper.cs
@@ -0,0 +1,172 @@
+using UnityEngine;
+
+namespace Khynan_Coding
+{
+    public static class AnimatorHelper
+    {
+        #region Trigger - Set
+        public static void SetAnimatorTriggerParameter(Animator animator, string triggerName)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return;
+            }
+
+            animator.SetTrigger(triggerName);
+        }
+        #endregion
+
+        #region Boolean - Get/Set
+        public static bool GetAnimatorBooleanParameter(Animator animator, string booleanName)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return false;
+            }
+
+            return animator.GetBool(booleanName);
+        }
+
+        public static void SetAnimatorBooleanParameter(Animator animator, string booleanName, bool value)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return;
+            }
+
+            animator.SetBool(booleanName, value);
+        }
+        #endregion
+
+        #region Float - Get/Set
+        public static float GetAnimatorFloatParameter(Animator animator, string floatParameterName)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return 0;
+            }
+
+            return animator.GetFloat(floatParameterName);
+        }
+
+        public static void SetAnimatorFloatParameter(Animator animator, string floatParameterName, float value, float dampTime = 0)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return;
+            }
+
+            animator.SetFloat(floatParameterName, value, dampTime, Time.deltaTime);
+        }
+        #endregion
+
+        #region Integer - Get/Set
+        public static int GetAnimatorIntParameter(Animator animator, string intParameterName)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return 0;
+            }
+
+            return animator.GetInteger(intParameterName);
+        }
+
+        public static void SetAnimatorIntParameter(Animator animator, string intParameterName, int value)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return;
+            }
+
+            animator.SetInteger(intParameterName, value);
+        }
+        #endregion
+
+        #region Layer
+        public static void SetAnimatorActiveLayer(Animator animator, int layerIndex, float layerWeight)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return;
+            }
+
+            animator.SetLayerWeight(layerIndex, layerWeight);
+        }
+        #endregion
+
+        #region Boolean - States
+        public static bool IsThisAnimationPlaying(Animator animator, string animationName)
+        {
+            if (!animator)
+            {
+                Debug.LogError("No animator.");
+                return false;
+            }
+
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName(animationName))
+            {
+                return true;
+            }
+
+            return false;
+        }
+        #endregion
+
+        public static void PlayThisAnimationOnThisLayer(Animator animator, int layerIndex, float layerWeight, string booleanName, bool value)
+        {
+            SetAnimatorActiveLayer(animator, layerIndex, layerWeight);
+            SetAnimatorBooleanParameter(animator, booleanName, value);
+        }
+
+        public static bool HasParameter(Animator animator, string paramName)
+        {
+            foreach (AnimatorControllerParameter param in animator.parameters)
+            {
+                if (param.name == paramName) return true;
+            }
+            return false;
+        }
+
+        public static void DebugAnimationsDuration(Animator animator)
+        {
+            if (!animator || animator.runtimeAnimatorController.animationClips.Length == 0) 
+            {
+                Debug.LogError("No animator or the list of animationClips is empty.");
+                return; 
+            }
+
+            for (int i = 0; i < animator.runtimeAnimatorController.animationClips.Length; i++)
+            {
+                Debug.Log("DEBUG | Index : " + i + " "
+                + animator.runtimeAnimatorController.animationClips[i].name
+                + " | Length : "
+                + animator.runtimeAnimatorController.animationClips[i].length + " | AverageDuration : "
+                + animator.runtimeAnimatorController.animationClips[i].length, animator.transform);
+            }
+        }
+
+        public static float GetAnimationLength(Animator animator, int animationIndex)
+        {
+            return animator.runtimeAnimatorController.animationClips[animationIndex].length;
+        }
+
+        public static void SetAnimatorOverrideController(Animator animator, AnimatorOverrideController aoc)
+        {
+            if (!animator) 
+            {
+                Debug.LogError("No animator");
+                return; 
+            }
+
+            animator.runtimeAnimatorController = aoc;
+        }
+    }
+}
